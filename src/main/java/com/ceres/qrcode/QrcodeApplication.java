1890efeef0676650b003816b43c11c947850fd03
artemis <xandu1738@gmail.com>
2023-05-09 10:50:03 +0300

    init

---
+++ src/main/java/com/ceres/qrcode/QrcodeApplication.java
@@ -0,0 +1,24 @@
+package com.ceres.qrcode;
+
+import com.google.zxing.WriterException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.ceres.qrcode.generator.QrGenerator.createQRImage;
+
+@SpringBootApplication
+public class QrcodeApplication {
+    public static void main(String[] args) throws IOException, WriterException {
+        SpringApplication.run(QrcodeApplication.class, args);
+        String qrCodeText = "https://www.amigoscode.com";
+        String filePath = "JD.png";
+        int size = 300;
+        String fileType = "png";
+        File qrFile = new File(filePath);
+        createQRImage(qrFile, qrCodeText, size, fileType);
+        System.out.println("DONE");
+    }
+}
---
+++ src/main/java/com/ceres/qrcode/generator/QrGenerator.java
@@ -0,0 +1,45 @@
+package com.ceres.qrcode.generator;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+public class QrGenerator {
+    public static void createQRImage(File qrFile, String qrCodeText, int size, String fileType)
+            throws WriterException, IOException {
+        Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<>();
+        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix byteMatrix = qrCodeWriter.encode(qrCodeText, BarcodeFormat.QR_CODE, size, size, hintMap);
+
+        int dimension = byteMatrix.getWidth();
+        BufferedImage image = new BufferedImage(dimension, dimension, BufferedImage.TYPE_INT_RGB);
+        image.createGraphics();
+
+        Graphics2D graphics = (Graphics2D) image.getGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, dimension, dimension);
+
+        graphics.setColor(Color.BLACK);
+
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                if (byteMatrix.get(i, j)) {
+                    graphics.fillRect(i, j, 1, 1);
+                }
+            }
+        }
+        ImageIO.write(image, fileType, qrFile);
+    }
+}
